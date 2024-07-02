bf1d918f282e6daf77b308963abd47d73faae338
Murilo <murilo.varges@gmail.com>
2024-07-02 12:22:01 -0300

    Implementado cadastro de cidade.

---
+++ ListarCidade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include('includes/conexao.php');
+        $sql = "SELECT * FROM cidade";        
+        $result = mysqli_query($con, $sql); // Executa a consulta
+    ?>
+    <h1>Consulta de Cidades</h1>
+    <a href="CadastroCidade.html">Cadastrar Nova Cidade</a>
+    <table align="center" border="1" width="500">
+        <tr>
+            <th>Código</th>
+            <th>Nome</th>
+            <th>Estado</th>
+        </tr>
+        <?php //mysqli_fetch_array lê uma linha por vez
+            while($row = mysqli_fetch_array($result)){
+                echo "<tr>";
+                echo "<td>".$row['id']."</td>";
+                echo "<td>".$row['nome']."</td>";
+                echo "<td>".$row['estado']."</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
